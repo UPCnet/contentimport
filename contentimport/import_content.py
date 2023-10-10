6611493671f4800b7d7098e4fe64aeaa15bdc8b2
Pilar Marinas <pilar.marinas@upcnet.es>
2023-10-10 11:13:12 +0200

    Quitar Try-except fix collection

--- contentimport/import_content.py
+++ contentimport/import_content.py
@@ -427,12 +427,8 @@ class CustomImportContent(ImportContent):
                 fixed_fields.append(field)
         if fixed_fields:
             item["customViewFields"] = fixed_fields
-        try:
-            item["query"] = fix_collection_query(item.pop("query", []))
-        except:
-            logger.info(f"Drop collection: {item['@id']}")
-            return
 
+        item["query"] = fix_collection_query(item.pop("query", []))
         if not item["query"]:
             logger.info(f"Drop collection without query: {item['@id']}")
             return
